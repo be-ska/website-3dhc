befb8b2f341b2d53037d2891f6c841dad2b85500
Luca Pescante <luca@pescante.it>
2025-08-12 12:37:22 +0200

    src/data: add pilots and sponsors

--- src/data.js
+++ src/data.js
@@ -1,68 +1,335 @@
-export const pilots = [
-  {
-    name: "Mirko Cesena",
-    country: "ITA (forse)",
-    age: "meno di me",
-    helicopter: {
-      manufacturer: "Align",
-      model: "T-Rex 600 Nitro"
-    },
-    sponsors: ["Align", "RCHeli", "Thunder Power"],
-    link: "https://www.facebook.com/mirko.cesena" // Add appropriate link
-  },
-  {
-    name: "Luca Pescante",
-    country: "ITA",
-    age: 29,
-    helicopter: {
-      manufacturer: "SAB",
-      model: "Goblin Kraken"
-    },
-    sponsors: ["SAB", "Castle Creations", "BK Servos"],
-    link: "https://www.facebook.com/luca.pescante" // Add appropriate link
-  },
-  {
-    name: "Pinucccc",
-    country: "France",
-    age: 28,
-    helicopter: {
-      manufacturer: "Align",
-      model: "T-Rex 700X"
-    },
-    sponsors: ["Align", "Mikado", "Scorpion"],
-    link: "https://www.facebook.com/pinucccc" // Add appropriate link
-  }
-];
-
 export const sponsors = [
+  // {
+  //   name: "Align",
+  //   image: "./sponsors/align.png",
+  //   link: "https://www.align.com.tw"
+  // },
+  // {
+  //   name: "Mikado",
+  //   image: "./sponsors/mikado.png",
+  //   link: "https://shop.mikado-heli.de"
+  // },
+  // {
+  //   name: "Goosky",
+  //   image: "./sponsors/goosky.png",
+  //   link: "https://www.goo-sky.com/"
+  // },
   {
-    name: "Align",
-    image: "./sponsors/align.png",
-    link: "https://www.align.com.tw"
-  },
-  {
-    name: "Mikado",
-    image: "./sponsors/mikado.png",
-    link: "https://shop.mikado-heli.de"
-  },
-  {
-    name: "Goosky",
-    image: "./sponsors/goosky.png",
-    link: "https://www.goo-sky.com/"
-  },
-  {
-    name: "Elyshop",
+    name: "Bizmodel - Elyshop",
     image: "./sponsors/elyshop.png",
     link: "https://www.elyshop.com/store/"
   },
+  // {
+  //   name: "HobbyWing",
+  //   image: "./sponsors/hw.png",
+  //   link: "https://www.hobbywing.com/en/"
+  // },
+  // {
+  //   name: "neXt simulator",
+  //   image: "./sponsors/next.png",
+  //   link: "https://www.cgm-online.com/rc-flight-simulator/rc-flight-simulator_e.html"
+  // },
   {
-    name: "HobbyWing",
-    image: "./sponsors/hw.png",
-    link: "https://www.hobbywing.com/en/"
-  },
-  {
-    name: "neXt simulator",
-    image: "./sponsors/next.png",
-    link: "https://www.cgm-online.com/rc-flight-simulator/rc-flight-simulator_e.html"
+    name: "XLPower",
+    image: "./sponsors/xlpower.png",
+    link: "http://www.xlpower-rc.com/"
   }
 ];
+
+export const pilots = [
+    {
+        name: "Jordi Basas Casas",
+        country: "Spain",
+        age: "36",
+        helicopter: {
+            manufacturer: "SAB Goblin",
+            model: "RAW 580"
+        },
+        sponsors: [""],
+        link: ""
+    },
+    {
+        name: "Valentin Fiorese",
+        country: "Germany",
+        age: "19",
+        helicopter: {
+            manufacturer: "Mikado",
+            model: "LOGO 700"
+        },
+        sponsors: [
+            "Mikado",
+            "Yge",
+            "Xnova",
+            "Kst",
+            "Maniax",
+            "1st-rc"
+        ],
+        link: "https://youtu.be/0zP1t7k3XcM?si=i6eQS_Vusg9tjBh-"
+    },
+    {
+        name: "Fabien Mauger",
+        country: "Suisse",
+        age: "52",
+        helicopter: {
+            manufacturer: "Steam",
+            model: "AK700"
+        },
+        sponsors: ["Fr Power"],
+        link: ""
+    },
+    {
+        name: "Gabriel Grundner",
+        country: "Austria",
+        age: "13",
+        helicopter: {
+            manufacturer: "Mikado",
+            model: "LOGO 700"
+        },
+        sponsors: ["Modellbau Lindinger", "Papa"],
+        link: "https://youtu.be/H92-kz-NYFg?si=1Ao8xjQ1yHfEyMnL"
+    },
+    {
+        name: "Tom Beckert",
+        country: "Germany",
+        age: "19",
+        helicopter: {
+            manufacturer: "OMPhobby",
+            model: "M7"
+        },
+        sponsors: [
+            "Xnovamotors",
+            "Gdw Servo",
+            "Bonka/mylipo",
+            "1st Rc"
+        ],
+        link: "https://www.facebook.com/share/1Ao4h8FyL7/"
+    },
+    {
+        name: "Antonio Orlando",
+        country: "Italia",
+        age: "36",
+        helicopter: {
+            manufacturer: "SAB",
+            model: "GOBLIN RAW 580"
+        },
+        sponsors: ["SAB", "Xnova", "Kavan", "Rcmodel"],
+        link: ""
+    },
+    {
+        name: "Vincenzo Claps",
+        country: "Italia",
+        age: "57",
+        helicopter: {
+            manufacturer: "Steam, Align",
+            model: "AK700, TB70"
+        },
+        sponsors: [""],
+        link: ""
+    },
+    {
+        name: "Lilian Many",
+        country: "France",
+        age: "18",
+        helicopter: {
+            manufacturer: "Goosky",
+            model: "RS7"
+        },
+        sponsors: [
+            "Goosky",
+            "Maniax",
+            "Kst",
+            "Azur Power",
+            "Egodrift",
+            "Hobbywing"
+        ],
+        link: "www.facebook.com/lilian.many.7?"
+    },
+    {
+        name: "Ludovic Abrignani",
+        country: "France",
+        age: "23",
+        helicopter: {
+            manufacturer: "Steam",
+            model: "STEAM AK700"
+        },
+        sponsors: ["Steam", "Fr Power"],
+        link: "https://www.instagram.com/ludovic_abrignani"
+    },
+    {
+        name: "Lorenzo Costantini",
+        country: "Italia",
+        age: "21",
+        helicopter: {
+            manufacturer: "Mikado",
+            model: "LOGO 700"
+        },
+        sponsors: [
+            "Rc-Model",
+            "Mikado Model Helicopters",
+            "Gens Ace"
+        ],
+        link: ""
+    },
+    {
+        name: "Mirko Menna",
+        country: "Italy",
+        age: "18",
+        helicopter: {
+            manufacturer: "Steam",
+            model: "AK700"
+        },
+        sponsors: ["Steam", "Frpower", "Xdfly", "Vulcan"],
+        link: ""
+    },
+    {
+        name: "Filippo Cicchelli",
+        country: "Italia",
+        age: "16",
+        helicopter: {
+            manufacturer: "OMPhobby",
+            model: "M7"
+        },
+        sponsors: [
+            "OMPhobby",
+            "Frpower",
+            "Xdlfy",
+            "Vulcan Rotors"
+        ],
+        link: ""
+    },
+    {
+        name: "Giorgio Cacciatore",
+        country: "Italia",
+        age: "37",
+        helicopter: {
+            manufacturer: "XLPower",
+            model: "SPECTER 700V2"
+        },
+        sponsors: [""],
+        link: ""
+    },
+    {
+        name: "Thula Vivek",
+        country: "Switzerland",
+        age: "29",
+        helicopter: {
+            manufacturer: "OMP",
+            model: "M7"
+        },
+        sponsors: [
+            "Egodrift",
+            "Yge",
+            "Agf",
+            "Azure",
+            "Bonka Battery My Lipo"
+        ],
+        link: ""
+    },
+    {
+        name: "Joël Franz",
+        country: "Switzerland",
+        age: "25",
+        helicopter: {
+            manufacturer: "OMPhobby",
+            model: "M7"
+        },
+        sponsors: [
+            "OMPhobby",
+            "Frpower",
+            "Kst",
+            "Egodrift",
+            "Azure"
+        ],
+        link:
+            "https://www.facebook.com/share/1Bx9hqyDFP/?" +
+            "mibextid=wwXIfr"
+    },
+    {
+        name: "Marco Caccin",
+        country: "Italia",
+        age: "39",
+        helicopter: {
+            manufacturer: "SAB",
+            model: "RAW 700"
+        },
+        sponsors: [""],
+        link: ""
+    },
+    {
+        name: "Axel Dumont",
+        country: "France",
+        age: "27",
+        helicopter: {
+            manufacturer: "XLPower",
+            model: "SPECTER 700 V2 WC"
+        },
+        sponsors: ["Frpower", "XLPower", "Vulcan"],
+        link: "https://www.facebook.com/axel.dumont.58"
+    },
+    {
+        name: "Alexandre Douchez",
+        country: "France",
+        age: "27",
+        helicopter: {
+            manufacturer: "XLPower",
+            model: "SPECTER 700 V2"
+        },
+        sponsors: ["XLPower", "Fr Power"],
+        link:
+            "https://www.instagram.com/alex_dchz59?" +
+            "igsh=MmVpcTc3a2txeWNq"
+    },
+    {
+        name: "Lucas Giraud",
+        country: "France",
+        age: "20",
+        helicopter: {
+            manufacturer: "Goosky",
+            model: "QQRS7"
+        },
+        sponsors: ["Goosky"],
+        link: ""
+    },
+    {
+        name: "Quentin Giraud",
+        country: "France",
+        age: "13",
+        helicopter: {
+            manufacturer: "Goosky",
+            model: "RS7"
+        },
+        sponsors: ["Goosky"],
+        link: ""
+    },
+    {
+        name: "Lucas Giraud",
+        country: "France",
+        age: "20",
+        helicopter: {
+            manufacturer: "Goosky",
+            model: "RS7"
+        },
+        sponsors: [
+            "Goosky",
+            "Kst",
+            "Xnova",
+            "Hobbywing",
+            "Radiomaster",
+            "Maniax",
+            "Azur Power"
+        ],
+        link: ""
+    },
+    {
+        name: "Quentin Giraud",
+        country: "France",
+        age: "13",
+        helicopter: {
+            manufacturer: "Goosky",
+            model: "RS7"
+        },
+        sponsors: ["Goosky"],
+        link: ""
+    }
+];
+
