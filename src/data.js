e7a611471e7d5ce42542e62eff15ef135e308f0c
Luca Pescante <luca@pescante.it>
2025-09-11 20:04:44 +0200

    src/data: update pilots and sponsors

--- src/data.js
+++ src/data.js
@@ -1,39 +1,59 @@
 export const sponsors = [
-  // {
-  //   name: "Align",
-  //   image: "./sponsors/align.png",
-  //   link: "https://www.align.com.tw"
-  // },
-  // {
-  //   name: "Mikado",
-  //   image: "./sponsors/mikado.png",
-  //   link: "https://shop.mikado-heli.de"
-  // },
-  // {
-  //   name: "Goosky",
-  //   image: "./sponsors/goosky.png",
-  //   link: "https://www.goo-sky.com/"
-  // },
-  {
-    name: "Bizmodel - Elyshop",
-    image: "./sponsors/elyshop.png",
-    link: "https://www.elyshop.com/store/"
-  },
-  // {
-  //   name: "HobbyWing",
-  //   image: "./sponsors/hw.png",
-  //   link: "https://www.hobbywing.com/en/"
-  // },
-  // {
-  //   name: "neXt simulator",
-  //   image: "./sponsors/next.png",
-  //   link: "https://www.cgm-online.com/rc-flight-simulator/rc-flight-simulator_e.html"
-  // },
-  {
-    name: "XLPower",
-    image: "./sponsors/xlpower.png",
-    link: "http://www.xlpower-rc.com/"
-  }
+    {
+        name: "SAB",
+        image: "./sponsors/sab.svg",
+        link: "https://sabitaly.it/en"
+    },
+    {
+        name: "Align",
+        image: "./sponsors/align.png",
+        link: "https://www.align.com.tw"
+    },
+    {
+        name: "XLPower",
+        image: "./sponsors/xlpower.png",
+        link: "http://www.xlpower-rc.com/"
+    },
+    {
+        name: "Ambrosio Costruzioni",
+        image: "./sponsors/ambrosio.png",
+        link: "https://www.ambrosiocostruzioni.it/"
+    },
+    {
+        name: "Bizmodel - Elyshop",
+        image: "./sponsors/elyshop.png",
+        link: "https://www.elyshop.com/store/"
+    },
+    {
+        name: "DAD Events",
+        image: "./sponsors/dad.png",
+        link: "https://www.dadeventssrl.it/",
+    },
+    {
+        name: "FRPower",
+            image: "./sponsors/fr.png",
+            link: "https://www.frpower.it/"
+    },
+    {
+        name: "RC Model",
+        image: "./sponsors/rcm.png",
+        link: "http://www.rc-model.it/"
+    },
+    // {
+    //     name: "Mikado",
+    //     image: "./sponsors/mikado.png",
+    //     link: "https://shop.mikado-heli.de"
+    // },
+    // {
+        //   name: "HobbyWing",
+        //   image: "./sponsors/hw.png",
+        //   link: "https://www.hobbywing.com/en/"
+        // },
+    // {
+        //   name: "neXt simulator",
+        //   image: "./sponsors/next.png",
+        //   link: "https://www.cgm-online.com/rc-flight-simulator/rc-flight-simulator_e.html"
+        // },
 ];
 
 export const pilots = [
@@ -107,7 +127,7 @@ export const pilots = [
             "1st Rc"
         ],
         link: "https://www.facebook.com/share/1Ao4h8FyL7/",
-        category: "Sport"
+        category: "Advanced"
     },
     {
         name: "Antonio Orlando",
@@ -169,8 +189,8 @@ export const pilots = [
         country: "Italia",
         age: "21",
         helicopter: {
-            manufacturer: "Mikado",
-            model: "LOGO 700"
+            manufacturer: "Align",
+            model: "TB70"
         },
         sponsors: [
             "Rc-Model",
@@ -328,5 +348,87 @@ export const pilots = [
         sponsors: ["Goosky"],
         link: "",
         category: "Sport"
+    },
+    {
+        name: "Andrei Tripolt",
+        country: "Austria",
+        age: "22",
+        helicopter: {
+            manufacturer: "XLPower",
+            model: "Specter 700 Ultimate"
+        },
+        sponsors: ["XL Power", "Egodrif", "Yg", "Theta", "Azur", "Helicoac", "Livehobb", "Hota", "Supra", "Mikado", "Maniax"],
+        link: "",
+        category: "Advanced"
+    },
+    {
+        name: "István Huszti",
+        country: "Hungary",
+        age: "36",
+        helicopter: {
+            manufacturer: "Steam",
+            model: "XL"
+        },
+        sponsors: ["Egodrift", "FRPower"],
+        link: "",
+        category: "FunFly"
+    },
+    {
+        name: "Dario Neuenschwander",
+        country: "Switzerland",
+        age: "37",
+        helicopter: {
+            manufacturer: "Tronhelicopters",
+            model: "TRON 7.0 ELITE"
+        },
+        sponsors: [
+            "Tronhelicopters",
+            "Spinblades"
+        ],
+        link: "",
+        category: "Sport"
+    },
+    {
+        name: "Michele Cimarelli",
+        country: "Italia",
+        age: "51",
+        helicopter: {
+            manufacturer: "SAB",
+            model: "GOBLIN 700"
+        },
+        sponsors: [""],
+        link: "",
+        category: "FunFly"
+    },
+    {
+        name: "Biagio Ambrosio",
+        country: "Italia",
+        age: "57",
+        helicopter: {
+            manufacturer: "SAB",
+            model: "RAW 580"
+        },
+        sponsors: [
+            "Rcmodel"
+        ],
+        link: "",
+        category: "Sport"
+    },
+    {
+        name: "Benjamin Britton",
+        country: "United Kingdom",
+        age: "30",
+        helicopter: {
+            manufacturer: "Goosky",
+            model: "RS7"
+        },
+        sponsors: [
+            "Goosky",
+            "Xdfly",
+            "Midland Helicopters",
+            "360 Technologies"
+        ],
+        link: "https://www.facebook.com/share/1Bjw2LjPw6/",
+        category: "Advanced"
     }
 ];
